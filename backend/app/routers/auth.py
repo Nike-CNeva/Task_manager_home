c4d312c45f94589a47bdfcb5a1dcc8e566c40eb5
NikeCNeva <nikecneva@gmail.com>
2025-05-15 09:00:33 +0300

    modified:   backend/app/routers/auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,21 +1,21 @@
-from fastapi import APIRouter, Body, Depends, Form, HTTPException, status
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
 from backend.app.core.dependencies import get_db
+from backend.app.schemas.user import UserBase
 from middlewares.auth_middleware import create_access_token, verify_password
-from backend.app.schemas.schemas import UserBase
 from services import user_service
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
 @router.post("/login", summary="Авторизация пользователя")
-def login_api(
+async def login_api(
     username: str = Body(...),
     password: str = Body(...),
-    db: Session = Depends(get_db)
+    db: AsyncSession = Depends(get_db)
 ):
     """Аутентификация пользователя, возвращает access токен"""
-    user = user_service.get_user_by_username(db, username)
+    user = await user_service.get_user_by_username(db, username)
 
     if user is None:
         raise HTTPException(
@@ -23,7 +23,7 @@ def login_api(
             detail="Неверный логин или пароль"
         )
 
-    if not verify_password(password, user.password):
+    if not await verify_password(password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Неверный логин или пароль"
@@ -33,13 +33,16 @@ def login_api(
     return JSONResponse(content={
         "access_token": access_token,
         "token_type": "bearer",
-        "user": UserBase.from_orm(user).dict()
+        "user": UserBase.model_validate(user).model_dump()
     })
 
+
 @router.post("/logout", summary="Выход из аккаунта")
-def logout_api():
-    """Сообщение об успешном выходе (токен удаляется на клиенте)"""
-    return JSONResponse(
+async def logout_api():
+    response = JSONResponse(
         content={"message": "Вы успешно вышли из системы."},
         status_code=status.HTTP_200_OK
     )
+    # Удаляем куку с токеном — max_age=0 или expires на прошедшую дату
+    response.delete_cookie(key="access_token")
+    return response
