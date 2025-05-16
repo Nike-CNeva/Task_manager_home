095cf85addfbdc775d8a0ed884354868f8e80fef
NikeCNeva <nikecneva@gmail.com>
2025-05-16 13:27:24 +0300

    modified:   backend/app/main.py 	modified:   backend/app/models/__init__.py 	modified:   backend/app/models/comment.py 	modified:   backend/requirements.txt

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,10 +1,10 @@
-from .enums import *
-from .association_table import *
-from .user import User
-from .bid import Bid, Customer
-from .task import Task, TaskWorkshop
-from .workshop import Workshop
-from .product import Product, Profile, Klamer, Bracket, ExtensionBracket, Cassette, LinearPanel
-from .material import Material, Sheets
-from .comment import Comment
-from .files import Files
+from backend.app.models.enums import *
+from backend.app.models.association_table import *
+from backend.app.models.user import User
+from backend.app.models.bid import Bid, Customer
+from backend.app.models.task import Task, TaskWorkshop
+from backend.app.models.workshop import Workshop
+from backend.app.models.product import Product, Profile, Klamer, Bracket, ExtensionBracket, Cassette, LinearPanel
+from backend.app.models.material import Material, Sheets
+from backend.app.models.comment import Comment
+from backend.app.models.files import Files
--- backend/app/models/comment.py
+++ backend/app/models/comment.py
@@ -7,7 +7,7 @@ class Comment(Base):
     __tablename__ = "comment"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     task_id: Mapped[int] = mapped_column(ForeignKey("task.id"), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     content: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     is_read: Mapped[bool] = mapped_column(Boolean, default=False)
