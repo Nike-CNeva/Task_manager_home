cda95cc9b40af40ea34d87d9e7a0324b1a2c8d29
Nike-CNeva <nike@cneva.ru>
2025-05-18 20:46:28 +0300

    modified:   frontend/src/utils/axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import store from '@/store';
 // Создание экземпляра axios с базовыми настройками
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Устанавливаем базовый URL для всех запросов
+  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json', // Стандартный заголовок
   }
