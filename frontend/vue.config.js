9ac8e11a254b0cd8394b017fbf9e819796d89fcb
Nike-CNeva <nike@cneva.ru>
2025-05-18 12:41:45 +0300

    modified:   frontend/vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,9 +5,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
+        pathRewrite: { '^/api': '' },
         logLevel: 'debug', // Добавь этот параметр для логирования
       },
     },
