abefcfab5de387a85235849bc3156ca321be0f79
NikeCNeva <nikecneva@gmail.com>
2025-05-30 13:44:10 +0300

    modified:   backend/app/schemas/product_fields.py

--- backend/app/schemas/product_fields.py
+++ backend/app/schemas/product_fields.py
@@ -32,15 +32,16 @@ product_fields_by_type = {
     ],
     ProductTypeEnum.SHEET: [],
 }
+
 fields = [{"name": "quantity", "label": "Количество", "type": "number"},
           {"name": "color", "label": "Цвет", "type": "text"},
           {"name": "painting", "label": "Красится?", "type": "checkbox"}]
+
 async def get_product_fields(product_type: str):
     try:
         enum_type = ProductTypeEnum(product_type)
-        field = product_fields_by_type.get(enum_type, [])
-        for item in fields:
-            field.append(item)
-        return list(field)  # вернём копию
+        # создаём новый список как конкатенацию
+        field = product_fields_by_type.get(enum_type, []) + fields
+        return field
     except ValueError:
         return list(fields)
